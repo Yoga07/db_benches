4f93f1ea5d2fc7067d22e8cc2263a62d107e5a93
Yoga07 <yogeshwar_1997@hotmail.com>
2022-06-07 23:23:35 +0530

    heed

--- src/main.rs
+++ src/main.rs
@@ -1,68 +1,57 @@
-use bytes::Bytes;
+use std::borrow::Cow;
+use bytes::{Buf, Bytes};
 use rand::rngs::OsRng;
 use rand::Rng;
 use rayon::current_num_threads;
 use std::sync::Arc;
 use std::time::Instant;
-
-use persy::{Config, Persy};
 use tokio::sync::RwLock;
 use tokio::task;
 
+use std::fs;
+use std::path::Path;
+use heed::{EnvOpenOptions, Database, RwTxn};
+use heed::types::*;
+
 #[tokio::main]
 async fn main() {
-    let mut write_handles = vec![];
-    let mut read_handles = vec![];
-    let stored_keys = Arc::new(RwLock::new(vec![]));
+    let mut stored_keys = vec![];
 
-    Persy::create("./db.persy").unwrap();
-    let db = Arc::new(RwLock::new(
-        Persy::open("./db.persy", Config::new()).unwrap(),
-    ));
+    fs::create_dir_all(Path::new("./db").join("bytemuck.mdb")).unwrap();
+    let env = EnvOpenOptions::new().open(Path::new("./db").join("bytemuck.mdb")).unwrap();
+
+    // we will open the default unnamed database
+    let heed_db: Database<OwnedType<i32>, Str> = env.create_database(None).unwrap();
+    let db = heed_db;
 
     let now = Instant::now();
 
-    for _ in 0..1000 {
-        let stored = stored_keys.clone();
-        let db = db.clone();
-        let handle = task::spawn(async move {
-            // 1mb random data chunk
-            let random_value = random_bytes(1024 * 1024);
-            let random_key = &random_value[0..32];
-            let key = &format!("{random_key:?}");
+    for i in 0..1000 {
+        // 1mb random data chunk
+        let random_value = random_bytes(1024 * 1024);
+        let random_key = &random_value[0..32];
+        let key = &format!("{random_key:?}");
 
-            let mut tx = db.write().await.begin().unwrap();
+        // opening a write transaction
+        let mut wtxn = env.write_txn().unwrap();
+        // TODO: Figure out how to pass Bytes
+        db.put(&mut wtxn, &i, &format!("{random_value:?}")).unwrap();
+        wtxn.commit().unwrap();
 
-            tx.create_segment(key).unwrap();
-            tx.insert(key, &random_value).unwrap();
-            let prepared = tx.prepare().unwrap();
-            prepared.commit().unwrap();
+        println!("Wrote {i:?}");
 
-            stored.write().await.push(key.clone());
-
-            // db.write().await.put(random_key, &random_value).unwrap();
-        });
-
-        write_handles.push(handle);
+        stored_keys.push(key.clone());
     }
-
-    futures::future::join_all(write_handles).await;
 
     let write = now.elapsed();
     println!("Write time: {write:?}");
 
-    for key in stored_keys.clone().read().await.iter() {
-        let db = db.clone();
-        let key_clone = key.clone();
-        let handle = task::spawn(async move {
-            let x = db.read().await.scan(&key_clone).unwrap();
-            // assert_eq!(read_value, key_clone.1);
-        });
-
-        read_handles.push(handle);
+    // for key in stored_keys {
+    for i in 0..1000 {
+        let mut rtxn = env.read_txn().unwrap();
+        // let _ = db.get(&rtxn, &key).unwrap().unwrap();
+        let _ = db.get(&rtxn, &i).unwrap().unwrap();
     }
-
-    futures::future::join_all(read_handles).await;
 
     let read = now.elapsed() - write;
     println!("Read time: {read:?}");
